734b1c4828922660166f7e46c3309d0971c90440
sophie19921005 <m18768120089@163.com>
2018-10-13 20:47:51 +0800

    完善models.py中的核心表

--- jobplus8-1/jobplus/app.py
+++ jobplus8-1/jobplus/app.py
@@ -1,13 +1,18 @@
-
+from flask_migrate import Migrate
 from flask import Flask, render_template, url_for
 from .config import configs
+from jobplus.models import db,User,Job,Company
+from flask_login import LoginManager
+
+def register_blueprints(app):
+    from .handlers import front
+    app.register_blueprint(front)
+    
 
 def create_app(config):
-	app = Flask(__name__)
-	app.config.from_object(configs.get(config))
-
-	@app.route('/test')
-	def test():
-		return render_template('index.html')
-
-	return app
+    app=Flask(__name__)
+    app.config.from_object(configs.get(config))
+    db.init_app(app)
+    Migrate(app,db)
+    register_blueprints(app)
+    return app
